02c19fa9c911f583217bcbf1458b13bd63fd1b2c
Bryan B <breatoz@me.com>
2016-05-09 22:31:12 -0400

    Permitted endpoints only.
    Highly suggest adapting this to exclusively allow programmed endpoints // claimed // identity

--- example.php
+++ example.php
@@ -1,7 +1,9 @@
 <?php
 require 'openid.php';
 try {
-    $openid = new LightOpenID;
+    ### Must provide the identiy and endpoint resources.
+    $openid = new LightOpenID(['permitted.identy' => 'http://steamcommunity.com/', 'permitted.endpoint' => 'https://steamcommunity.com/']);
+        
     if(!$openid->mode) {
         if(isset($_POST['openid_identifier'])) {
             $openid->identity = $_POST['openid_identifier'];
